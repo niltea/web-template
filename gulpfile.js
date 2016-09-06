553e9cf1e16383465e3e18a551a5fce84ec3a629
貴志 祐輔 <kishi@balcolony.com>
2016-09-06 19:22:45 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var browser = require("browser-sync");
+var plumber = require("gulp-plumber");
+var pug = require("gulp-pug");
+var uglify = require("gulp-uglify");
+
+gulp.task("default",['server'], function() {
+	gulp.watch(["src/js/**/*.js","!src/js/min/**/*.js"],["js"]);
+	gulp.watch("src/sass/**/*.scss",["sass"]);
+	gulp.watch(["src/pug/**/*.pug"],["pug"]);
+});
+
+gulp.task("server", function() {
+	browser({
+		server: {
+			baseDir: "./public/"
+		}
+	});
+});
+gulp.task("sass", function() {
+	gulp.src("src/sass/**/*.scss")
+	.pipe(plumber())
+	.pipe(sass({
+		outputStyle: "compressed"
+	}))
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions']
+	}))
+	.pipe(gulp.dest("./public/css"))
+	.pipe(browser.reload({stream:true}));
+});
+gulp.task("pug", function() {
+	gulp.src(
+		["src/pug/**/*.pug", "!pug/**/_*.pug"]
+		)
+	.pipe(plumber())
+	.pipe(pug({},{"ext": ".html"}))
+	.pipe(gulp.dest("./public/"))
+	.pipe(browser.reload({stream:true}));
+});
+gulp.task("js", function() {
+    gulp.src(["src/js/**/*.js","!src/js/min/**/*.js"])
+        .pipe(uglify())
+        .pipe(gulp.dest("./public/js"));
+});
